fa708a92c42071b1bd0e8dd9c21fd195b549e739
Gerald4102 <106319833+Gerald4102@users.noreply.github.com>
2023-03-06 17:26:02 +0000

    fixed addPagination for meets expectations

--- js/script.js
+++ js/script.js
@@ -15,38 +15,34 @@ const studentsPerPage = 9;
 const numberOfPages = Math.ceil(data.length /studentsPerPage);
 const linkList = document.querySelector('.link-list');
 let activePage = 1;
+
+
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 function showPage(students) {
-   let page = 5;
-   if ( page === 5 ) {
-      const firstStudent = (page * studentsPerPage) - studentsPerPage;
-      let lastStudent = 0;
-      if ( page*studentsPerPage < data.length ) {
-         lastStudent = page*studentsPerPage;
-      } else {
-         lastStudent = data.length;
-      }
-          console.log(firstStudent);
-          console.log(lastStudent);
-          console.log(data.length);
-      for( let i=firstStudent; i<lastStudent; i++ ) {
-         studentList.insertAdjacentHTML( 'beforeend', 
-         `
-            <li class="student-item cf">
-               <div class="student-details">
-                  <img class="avatar" src="${students[i].picture.large}" alt="Profile Picture">
-                  <h3>${students[i].name.first} ${students[i ].name.last}</h3>
-                  <span class="email">${students[i].email}</span>
-               </div>
-               <div class="joined-details">
-                  <span class="date">Joined ${students[i].registered.date}</span>
-               </div>
-            </li>
-         `);
-      }
+   const firstStudent = (activePage * studentsPerPage) - studentsPerPage;
+   let lastStudent = 0;
+   if ( activePage*studentsPerPage < data.length ) {
+      lastStudent = activePage*studentsPerPage;
+   } else {
+      lastStudent = data.length;
+   }
+   for( let i=firstStudent; i<lastStudent; i++ ) {
+      studentList.insertAdjacentHTML( 'beforeend', 
+      `
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${students[i].picture.large}" alt="Profile Picture">
+               <h3>${students[i].name.first} ${students[i ].name.last}</h3>
+               <span class="email">${students[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${students[i].registered.date}</span>
+            </div>
+         </li>
+      `);
    }
 }
 
@@ -56,15 +52,17 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 function addPagination(students) {
-for ( let i=0; i<numberOfPages; i++ ) {
-   const button = document.createElement('button');
-   button.type = 'button';
-   button.textContent = i+1;
-   if ( i===0 ) {
-      button.className = 'active';
-   };
-   linkList.append(button);
-}
+   for ( let i=0; i<numberOfPages; i++ ) {
+      const li = document.createElement('li');
+      const button = document.createElement('button');
+      button.type = 'button';
+      button.textContent = i+1;
+      if ( i===0 ) {
+         button.className = 'active';
+      };
+      li.append(button)
+      linkList.append(li);
+   }
 }
 
 
@@ -73,7 +71,16 @@ addPagination(data);
 showPage(data);
 
 linkList.addEventListener('click', (event)=> {
-   activePage = event.target.textContent;
-   console.log(activePage);
+   if(event.target.type === 'button') {
+      activePage = event.target.textContent;
+      const buttons = linkList.querySelectorAll('button');
+      for ( let i=0; i<numberOfPages; i++ ) {
+         buttons[i].className = '';
+         if ( i === activePage-1 ) {
+            buttons[i].className = 'active';
+         }
+      }
+      studentList.innerHTML = '';
+      showPage(data);
+   }
 });
-
